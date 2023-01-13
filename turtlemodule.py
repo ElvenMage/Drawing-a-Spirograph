5bb0dcb6ed9e6224687634d922fb3ebb17804115
Emin <114852408+ElvenMage@users.noreply.github.com>
2023-01-13 12:07:52 +0300

    Add files via upload

---
+++ turtlemodule.py
@@ -0,0 +1,32 @@
+import random
+import turtle as t
+from turtle import Screen
+
+timmy = t.Turtle()
+t.colormode(255)
+
+
+def random_color():
+    r = random.randint(0, 255)
+    g = random.randint(0, 255)
+    b = random.randint(0, 255)
+    random_colour = (r, g, b)
+    return random_colour
+
+
+timmy.speed("fastest")
+
+
+def draw_spirograph(size_of_gap):
+    for _ in range(int(360 / size_of_gap)):
+        timmy.color(random_color())
+        timmy.circle(100)
+        timmy.setheading(timmy.heading() + size_of_gap)
+        timmy.circle(100)
+
+
+draw_spirograph(15)
+
+
+screen = Screen()
+screen.exitonclick()
