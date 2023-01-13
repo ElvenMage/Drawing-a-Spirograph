49002d6bcc0d2275618e0c109336313d3e086704
Emin <114852408+ElvenMage@users.noreply.github.com>
2023-01-13 12:10:05 +0300

    Update TurtleSpirograph.py

--- TurtleSpirograph.py
+++ TurtleSpirograph.py
@@ -3,9 +3,9 @@ import turtle as t
 from turtle import Screen
 
 timmy = t.Turtle()
+
+# Defining the colors as rgb
 t.colormode(255)
-
-
 def random_color():
     r = random.randint(0, 255)
     g = random.randint(0, 255)
@@ -13,18 +13,19 @@ def random_color():
     random_colour = (r, g, b)
     return random_colour
 
-
+# Speed of the turtle
 timmy.speed("fastest")
 
 
 def draw_spirograph(size_of_gap):
     for _ in range(int(360 / size_of_gap)):
         timmy.color(random_color())
+        #you can adjust the radius of the circle
         timmy.circle(100)
         timmy.setheading(timmy.heading() + size_of_gap)
         timmy.circle(100)
 
-
+#You can adjust the gap as you wish
 draw_spirograph(15)
 
 
